a3d83deeecb46760a552d91c1096394a21803b7c
kfoq7 <frbrizoi@gmail.com>
2026-01-09 11:28:50 -0500

    feat(web): added search location by name or nearby location api

--- apps/don-emiliano-web/src/stores/location.ts
+++ apps/don-emiliano-web/src/stores/location.ts
@@ -1,14 +1,11 @@
-import { persistentAtom } from '@nanostores/persistent'
+import { persistentAtom, persistentBoolean } from '@nanostores/persistent'
 
 export type Location = {
   lat: number
   lng: number
 }
 
-export const delivery = persistentAtom<boolean>('delivery', false, {
-  encode: JSON.stringify,
-  decode: JSON.parse,
-})
+export const delivery = persistentBoolean('delivery', false)
 
 export const location = persistentAtom<Location | null>('location', null, {
   encode: JSON.stringify,
